f4190976593ca75489ba9013276019cabd377e81
shimochan1213 <shimokawashadefuel@gmail.com>
2020-10-08 21:58:29 +0900

    Initial Commit

---
+++ FireStoreEitangoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FireStoreEitangoApp
+//
+//  Created by 下川勇輝 on 2020/10/08.
+//  Copyright © 2020 Yuki Shimokawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
